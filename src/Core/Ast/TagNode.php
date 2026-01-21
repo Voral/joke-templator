a735073cd011e74d956f13f5a5cdf7a1811f513d
Alexander Vorobyev <voral.adm@gmail.com>
2026-01-21 21:53:12 +0300

    feat: Парсер AST-дерева

--- src/Core/Ast/TagNode.php
+++ src/Core/Ast/TagNode.php
@@ -11,16 +11,17 @@ class TagNode implements NodeInterface
 {
     /**
      * @param string $tagName Имя узла, без префикса шаблонизатора
-     * @param array<string,string> $attributes Атрибут узла в виде key=>value
+     * @param string $fullTagName Полное имя с префиксом шаблонизатора
+     * @param array<string,string|bool> $attributes Атрибут узла в виде key=>value
      * @param array $children Дочерние узлы
      * @param bool $selfClosing Признак самозакрывающегося тега
      */
     public function __construct(
         public string $tagName,
+        public string $fullTagName,
         public array $attributes = [],
         public array $children = [],
         public bool $selfClosing = false
-    )
-    {
+    ) {
     }
 }
