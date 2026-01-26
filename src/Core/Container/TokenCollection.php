f93f2cb6771a33a4a3637bb9ed82bd7ac4ad0dbf
Alexander Vorobyev <voral.adm@gmail.com>
2026-01-26 16:11:50 +0300

    feat: Отказался от сортировки коллекции описаний токенов

--- src/Core/Container/TokenCollection.php
+++ src/Core/Container/TokenCollection.php
@@ -12,73 +12,58 @@ use Vasoft\Joke\Templator\Exceptions\TemplatorException;
 class TokenCollection
 {
     /**
-     * @var array<string,TokenCollectionItem> Реестр описаний токенов
+     * @var array<string,TokenDescriptor> Реестр описаний токенов
      */
-    private array $items = [];
-    /** @var list<TokenDescriptor> Подготовленный список описаний токенов */
     private array $descriptors = [];
 
     /**
      * Добавляет описание токена с проверкой на существование
      * @param TokenDescriptor $descriptor Описание токена
-     * @param int $sort Порядок проверки токена лексером
      * @return $this
      * @throws TemplatorException Если токен с таким открывающим маркером уже существует
      */
-    public function add(TokenDescriptor $descriptor, int $sort): static
+    public function add(TokenDescriptor $descriptor): static
     {
-        if (array_key_exists($descriptor->open, $this->items)) {
+        if (array_key_exists($descriptor->open, $this->descriptors)) {
             throw new TemplatorException('Token already exists');
         }
-        $this->items[$descriptor->open] = new TokenCollectionItem($descriptor, $sort);
+        $this->descriptors[$descriptor->open] = $descriptor;
         return $this;
     }
 
     /**
      * Безусловное добавляет описание токена. Если токен с таким же открывающим маркером был уже зарегистрирован, то перезаписывается
      * @param TokenDescriptor $descriptor Описание токена
-     * @param int $sort Порядок проверки токена лексером
      * @return $this
      */
-    public function upsert(TokenDescriptor $descriptor, int $sort): static
+    public function upsert(TokenDescriptor $descriptor): static
     {
-        $this->items[$descriptor->open] = new TokenCollectionItem($descriptor, $sort);
+        $this->descriptors[$descriptor->open] = $descriptor;
         return $this;
     }
 
-    /**
-     * Получение отсортированного списка описаний токенов
-     *
-     * По умолчанию список подготавливается(сортируется) при первом вызове. Далее возвращается тот же список.
-     * Если передать true в качестве значения параметра то подготовка списка производится при этом вызове.
-     * @param bool $reset Флаг безусловной подготовки списка
-     * @return list<TokenDescriptor>
-     */
-    public function list(bool $reset = false): array
-    {
-        if ($reset || empty($this->descriptors)) {
-            uasort($this->items, static fn(TokenCollectionItem $a, TokenCollectionItem $b) => $a->sort <=> $b->sort);
-            $this->descriptors = [];
-            foreach ($this->items as $item) {
-                $this->descriptors[] = $item->descriptor;
-            }
-        }
-        return $this->descriptors;
-    }
 
     /**
      * Полная замена списка
-     * @param list<TokenCollectionItem> $items
+     * @param list<TokenDescriptor>|array<string,TokenDescriptor> $items
      * @return $this
      */
     public function reset(array $items): static
     {
-        $this->items = [];
         $this->descriptors = [];
-        foreach ($items as $item) {
-            $this->items[$item->descriptor->open] = $item;
+        foreach ($items as $descriptor) {
+            $this->descriptors[$descriptor->open] = $descriptor;
         }
 
         return $this;
+    }
+
+    /**
+     * Возвращает список описаний токенов
+     * @return array<string,TokenDescriptor>
+     */
+    public function list(): array
+    {
+        return $this->descriptors;
     }
 }
--- tests/Core/Container/TokenCollectionTest.php
+++ tests/Core/Container/TokenCollectionTest.php
@@ -4,7 +4,6 @@ namespace Vasoft\Joke\Templator\Tests\Core\Container;
 
 use PHPUnit\Framework\TestCase;
 use Vasoft\Joke\Templator\Core\Container\TokenCollection;
-use Vasoft\Joke\Templator\Core\Container\TokenCollectionItem;
 use Vasoft\Joke\Templator\Core\Lexer\PrintToken;
 use Vasoft\Joke\Templator\Core\Lexer\StatementToken;
 use Vasoft\Joke\Templator\Core\Lexer\TokenDescriptor;
@@ -15,85 +14,57 @@ class TokenCollectionTest extends TestCase
     public function testAdd(): void
     {
         $collection = new TokenCollection();
-        $collection->add(new TokenDescriptor('{{', '}}', PrintToken::class), 100);
-        self::assertCount(1,$collection->list());
+        $collection->add(new TokenDescriptor('{{', '}}', PrintToken::class));
+        self::assertCount(1, $collection->list());
     }
+
     public function testAddTwice(): void
     {
         $collection = new TokenCollection();
-        $collection->add(new TokenDescriptor('{{', '}}', PrintToken::class), 100);
+        $collection->add(new TokenDescriptor('{{', '}}', PrintToken::class));
         self::expectException(TemplatorException::class);
         self::expectExceptionMessage('Token already exists');
-        $collection->add(new TokenDescriptor('{{', '}}', StatementToken::class), 100);
+        $collection->add(new TokenDescriptor('{{', '}}', StatementToken::class));
     }
+
+    public function testUpsert(): void
+    {
+        $collection = new TokenCollection();
+        $descriptor = new TokenDescriptor('{{', '}}', StatementToken::class);
+        $collection->add(new TokenDescriptor('{{', '}}', PrintToken::class));
+        $collection->upsert($descriptor);
+        $list = $collection->list();
+        self::assertCount(1, $list);
+        self::assertSame($descriptor, $list['{{']);
+    }
+
     public function testAddSeveral(): void
     {
         $collection = new TokenCollection();
         $collection->add(new TokenDescriptor('{{', '}}', PrintToken::class), 100);
         $collection->add(new TokenDescriptor('{%', '%}', StatementToken::class), 100);
-        self::assertCount(2,$collection->list());
+        self::assertCount(2, $collection->list());
     }
 
-    public function testSorted(): void
-    {
-        $collection = new TokenCollection();
-        $collection->add(new TokenDescriptor('{9', '}}', PrintToken::class), 9);
-        $collection->add(new TokenDescriptor('{8a', '%}', StatementToken::class), 8);
-        $collection->add(new TokenDescriptor('{8b', '%}', StatementToken::class), 8);
-        $collection->add(new TokenDescriptor('{5', '%}', StatementToken::class), 5);
-        $collection->add(new TokenDescriptor('{6', '%}', StatementToken::class), 6);
-        $sorted = $collection->list();
 
-        self::assertCount(5,$sorted);
-        self::assertSame('{5',$sorted[0]->open);
-        self::assertSame('{6',$sorted[1]->open);
-        self::assertContains($sorted[2]->open,['{8a','{8b']);
-        self::assertContains($sorted[3]->open,['{8a','{8b']);
-        self::assertSame('{9',$sorted[4]->open);
-    }
-    public function testSortedOnceAndResetAndUpsert(): void
-    {
-        $descriptor = new TokenDescriptor('{5', '%}', StatementToken::class);
-        $collection = new TokenCollection();
-        $collection->add(new TokenDescriptor('{9', '}}', PrintToken::class), 9);
-        $collection->add($descriptor, 5);
-
-        $sorted = $collection->list();
-        $collection->add(new TokenDescriptor('{1', '}}', PrintToken::class), 1);
-        $collection->upsert($descriptor, 10);
-
-        self::assertCount(2,$sorted);
-        self::assertSame('{5',$sorted[0]->open);
-        self::assertSame('{9',$sorted[1]->open);
-
-        $sorted = $collection->list();
-        self::assertCount(2,$sorted, 'Sorted twice');
-
-        $sorted = $collection->list(true);
-        self::assertCount(3,$sorted);
-        self::assertSame('{1',$sorted[0]->open);
-        self::assertSame('{9',$sorted[1]->open);
-        self::assertSame('{5',$sorted[2]->open);
-    }
     public function testReset(): void
     {
         $collection = new TokenCollection();
-        $collection->add(new TokenDescriptor('{9', '}}', PrintToken::class), 9);
-        $collection->add(new TokenDescriptor('{1', '}}', PrintToken::class), 1);
-        $sorted = $collection->list();
-        self::assertCount(2,$sorted);
+        $collection->add(new TokenDescriptor('{9', '}}', PrintToken::class));
+        $collection->add(new TokenDescriptor('{1', '}}', PrintToken::class));
+        self::assertCount(2, $collection->list());
 
         $collection->reset([
-            new TokenCollectionItem(new TokenDescriptor('{12', '}}', PrintToken::class), 12),
-            new TokenCollectionItem(new TokenDescriptor('{11', '}}', PrintToken::class), 11),
-            new TokenCollectionItem(new TokenDescriptor('{15', '}}', PrintToken::class), 15),
+            new TokenDescriptor('{12', '}}', PrintToken::class),
+            new TokenDescriptor('{11', '}}', PrintToken::class),
+            new TokenDescriptor('{15', '}}', PrintToken::class),
+            new TokenDescriptor('{12', '12}', PrintToken::class),
         ]);
-
-        $sorted = $collection->list();
-        self::assertCount(3,$sorted);
-        self::assertSame('{11',$sorted[0]->open);
-        self::assertSame('{12',$sorted[1]->open);
-        self::assertSame('{15',$sorted[2]->open);
+        /** @var array<string,TokenDescriptor> $list */
+        $list = $collection->list();
+        self::assertCount(3, $list);
+        self::assertSame(['{12', '{11', '{15'], array_keys($list));
+        self::assertSame('12}', $list['{12']->close);
     }
 
 }
